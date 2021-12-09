a014bd8869904eeabb0ef014f2b7b25a3e029039
GabrielSantoss <gabrazil03@hotmail.com>
2021-12-09 08:51:12 -0300

    Payment, one-to-one association

--- src/main/java/com/glipsolutions/curse/config/TestConfig.java
+++ src/main/java/com/glipsolutions/curse/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.glipsolutions.curse.entities.Category;
 import com.glipsolutions.curse.entities.Order;
 import com.glipsolutions.curse.entities.OrderItem;
+import com.glipsolutions.curse.entities.Payment;
 import com.glipsolutions.curse.entities.Product;
 import com.glipsolutions.curse.entities.User;
 import com.glipsolutions.curse.entities.enums.OrderStatus;
@@ -80,6 +81,11 @@ public class TestConfig implements CommandLineRunner{
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1 );
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 	
 	
